df22d90934846c40ead42f867877055da33f77e5
pelegs <pelegs@gmail.com>
2025-12-27 19:07:14 +0100

    Re implemented structure-to-compile determination

--- compile.py
+++ compile.py
@@ -1,15 +1,15 @@
 import argparse
+import copy
 import pathlib
 import subprocess
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--chapters", "-c", type=str, default="ALL")
-parser.add_argument("--sections", "-s", type=str, default="ALL")
+parser.add_argument("--structure", "-s", type=str, default="ALL")
 args = parser.parse_args()
 
 rootDir = pathlib.Path("./chapters")
 chapters_all = sorted(list(rootDir.iterdir()))
-book_structure = {
+full_book_structure = {
     chapter.name: [
         section.stem
         for section in sorted(list(chapter.iterdir()))
@@ -18,28 +18,27 @@ book_structure = {
     for chapter in chapters_all
 }
 
-chapters_to_compile = book_structure
-if args.chapters != "ALL":
-    chapters_from_args = args.chapters.split(",")
-    chapters_to_compile = {
-        chapter: book_structure[chapter]
-        for chapter in chapters_from_args
-        if chapter in book_structure.keys()
+structure_to_compile = copy.deepcopy(full_book_structure)
+if args.structure != "ALL":
+    chapters_list = args.structure.split(";")
+    structure_to_compile = {
+        chapter_name: [
+            section
+            for section in chapter.split(":")[1].split(",")
+            if section in full_book_structure[chapter_name]
+        ]
+        for chapter in chapters_list
+        if (chapter_name := chapter.split(":")[0]) in full_book_structure.keys()
     }
 
-    if args.sections != "ALL":
-        first_chapter_to_compile = chapters_from_args[0]
-        sections_in_first_chapter = book_structure[first_chapter_to_compile]
-        chapters_to_compile = {
-            first_chapter_to_compile: [
-                section
-                for section in sections_in_first_chapter
-                if section in args.sections.split(",")
-            ]
-        }
-
 print("The following chapters and sections will be compiled:")
-for chapter, sections in chapters_to_compile.items():
+for chapter, sections in structure_to_compile.items():
     print(f"* {chapter}")
     for section in sections:
         print(f" - {section}")
+
+# file = "main/main"
+# job = "wip"
+# chapter = "2_theBasics"
+# section = "2_linearTrans"
+# latexmk_cmd = f'latexmk -pdflua -shell-escape -jobname=main/{job} -pretex="\def\wipchapter{chapter}\def\wipsection{section}" -usepretex {file}'
